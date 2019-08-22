2a951de581f95b33f9778973fd24738f997a8e87
Pi <pi@abersailbot.co.uk>
2019-08-22 15:26:09 +0000

    updated deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-apt install hostapd gpsd mc git mosh ntp gpsd-clients screen tmux python-pip python-setuptools python-wheel bdist-wheel isc-dhcp-server vim-nox lsof
+apt install hostapd gpsd mc git mosh ntp gpsd-clients screen tmux python-pip python-setuptools python-wheel bdist-wheel isc-dhcp-server vim-nox lsof tcpdump
 cp gpsd /etc/default/gpsd
 cp boatd-config.yaml /etc/
 cp interfaces /etc/network/
@@ -10,9 +10,36 @@ cp isc-dhcp-server /etc/default/isc-dhcp-server
 cp dhcpd.conf /etc/dhcp/dhcpd.conf
 cp cmdline.txt /boot/cmdline.txt
 
+mkdir /var/log/boatd
+
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 
 
 #tmux wants the US locale
 echo "enable en_US UTF8 locale"
 dpkg-reconfigure locales
+
+
+#install platformio
+wget https://raw.githubusercontent.com/platformio/platformio/develop/scripts/get-platformio.py
+python get-platformio.py
+#put platformio in the path
+echo "export PATH=$PATH:/home/pi/.local/bin/" >> ~/.bashrc
+
+export PATH=$PATH:/home/pi/.local/bin/
+
+
+sudo pip install python-boatdclient
+
+cd /home/pi
+git clone --recursive https://github.com/abersailbot/dewi
+cd dewi
+cd boatd
+sudo python setup.py install
+cd ..
+cd dewi-boatd-driver
+sudo ln -s dewi_boatd_driver.py /usr/local/lib/python2.7/dist-packages
+
+#gopro stuff, not sure if this is needed?
+sudo pip install gopro goprocam
+sudo apt install python-opencv
