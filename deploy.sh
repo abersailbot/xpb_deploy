364936b740acb6ac5b8510c42b67c1361a7a4fb5
Colin Sauze <cos@aber.ac.uk>
2019-10-23 14:09:11 +0100

    switching to python3 and adding some default SSH keys

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ if [ `whoami` != "root" ] ; then
 fi
 
 apt -y update
-apt -y install hostapd gpsd mc git mosh ntp gpsd-clients screen tmux python-pip python-setuptools python-wheel isc-dhcp-server vim-nox lsof tcpdump libyaml-dev
+apt -y install hostapd gpsd mc git mosh ntp gpsd-clients screen tmux isc-dhcp-server vim-nox lsof tcpdump libyaml-dev python3-pip
 apt -y upgrade
 
 git clone --recursive https://github.com/abersailbot/xpb
@@ -52,13 +52,17 @@ echo "export PATH=$PATH:/home/pi/.local/bin/" >> ~/.bashrc
 
 export PATH=$PATH:/home/pi/.local/bin/
 
+#install python3 gpsd stuff
+pip3 install gps
 #install boatd
-pip install python-boatdclient
+pip3 install python-boatdclient
 
 cd /home/pi/xpb
 cd boatd
-sudo python setup.py install
+python3 setup.py install
 cd /home/pi/xpb/xpb_deploy
+#change boatd to run in python3
+sudo sed -i 's@^#!/usr/bin/python$@#!/usr/bin/python3@' /usr/local/bin/boatd
 
 #install boatd service
 cp boatd.service /etc/systemd/system/boatd.service
@@ -66,11 +70,18 @@ systemctl daemon-reload
 systemctl enable boatd
 
 #symlink driver
-ln -s /home/pi/xpb/xpb-boatd-driver/xpb_boatd_driver.py /usr/local/lib/python2.7/dist-packages
+ln -s /home/pi/xpb/xpb-boatd-driver/xpb_boatd_driver.py /usr/local/lib/python3.7/dist-packages
 
 #set the password to something more secure
 usermod -p '$6$2VfUJBiCiUNV/RkL$gz2TUtullYN2svx6jb39UESyOholUdE/EehNoqCKagEpzfJMS1wK9hOr1BkQpSMXbbu4Pmr8Pli6zanQ.g10Q0' pi
 
+#install SSH keys
+mkdir /home/pi/.ssh
+chmod 700 /home/pi/.ssh
+cp /home/pi/xpb/xpb_deploy/authorized_keys /home/pi/.ssh/authorized_keys
+chmod 600  /home/pi/.ssh/authorized_keys
+
 #set the timezone
 rm /etc/localtime
 ln -s /usr/share/zoneinfo/Etc/UTC /etc/localtime
+
